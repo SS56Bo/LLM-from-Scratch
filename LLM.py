ef527200ae108d8c3f67b7068f97e193a2a8ee80
SS56Bo <sohamsarkar345@gmail.com>
2025-09-10 16:07:05 +0530

    DataLoader prep before embedding starts

--- LLM.py
+++ LLM.py
@@ -15,5 +15,18 @@ class GPTDataLoader:
     def __len__(self):
         return len(self.input_ids)
     
-    def create_dataloader():
-        
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+    
+    @staticmethod
+    def create_dataloader(text: list[int], batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, workers=0):
+        data = GPTDataLoader(text, max_length, stride)
+        dataload = DataLoader(
+            data,
+            batch_size=batch_size,
+            shuffle=shuffle,
+            drop_last=drop_last,
+            num_workers=workers
+        )
+
+        return dataload
--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from LLM import *
 
 txt = Text()
 token = Tokenizer()
+
 raw_text = txt.extract_text("Rough.pdf")
 raw_tokens = token.tokenize_strings(raw_text)
 
 print(f"Token length: {len(raw_tokens)}")
+result_data = GPTDataLoader.create_dataloader(text=raw_tokens, batch_size=8, max_length=4, stride=4 ,shuffle=False)
+result_iter = iter(result_data)
+first_batch = next(result_iter)
+second_batch = next(result_iter)
+print(first_batch)
+print(second_batch)
